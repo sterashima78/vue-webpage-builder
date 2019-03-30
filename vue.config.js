25726662a1c19c95a67abd3ef30d010cae158111
Terashima Shin <sterashima78@gmail.com>
2019-03-30 11:59:50 +0900

    fi xsetting

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   runtimeCompiler: true,
   transpileDependencies: ["vuex-module-decorators"],
-  baseUrl: process.env.NODE_ENV === 'production:github'
+  baseUrl: process.env.DEPLOY === 'github'
     ? '/vue-webpage-builder/'
     : '/'
   
