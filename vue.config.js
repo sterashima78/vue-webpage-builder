90a9e9377c9a06bc96dba59734db2a68411e2cd1
Terashima Shin <sterashima78@gmail.com>
2019-03-30 11:56:26 +0900

    fix baseUrl

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   runtimeCompiler: true,
   transpileDependencies: ["vuex-module-decorators"],
   baseUrl: process.env.NODE_ENV === 'production:github'
-    ? '/vue-web-designer/'
+    ? '/vue-webpage-builder/'
     : '/'
   
 };
