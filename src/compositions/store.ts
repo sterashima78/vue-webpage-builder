9f9584235e48918b532b1243f912b4cf11870258
sterashima78 <sterashima78@gmail.com>
2020-02-23 08:35:11 +0000

    wip typing

---
+++ src/compositions/store.ts
@@ -0,0 +1,14 @@
+import { ref, Ref } from "@vue/composition-api";
+import { VNode } from "vue";
+import { NodeTree } from "@/types";
+import { make } from "fp-ts/lib/Tree";
+export const useState = () => {
+  const node: Ref<NodeTree> = ref(
+    make({
+      id: "root",
+      tag: "div",
+      text: ""
+    })
+  );
+  return node;
+};
---
+++ src/types.ts
@@ -0,0 +1,28 @@
+import { Tree } from "fp-ts/lib/Tree";
+import { VNode } from "vue";
+
+type NodeId = string & { readonly _brand: unique symbol };
+export type createNodeId = () => NodeId;
+
+type NodeAttributes = object & { readonly _brand: unique symbol };
+export type toAttributes = (attr: object) => NodeAttributes;
+
+interface Node {
+  id: string;
+  tag: string;
+  text: string;
+  attributes?: {
+    [name: string]: string;
+  };
+  classes?: string[];
+  on?: {
+    [event: string]: (event: any) => void;
+  };
+  domProps?: {
+    [name: string]: string;
+  };
+}
+
+export type NodeTree = Tree<Node>;
+
+export type toVNode = (node: Node) => VNode;
