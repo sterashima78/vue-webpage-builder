8738b713a5bb64ea6d6e226708cc2adcefa157a8
sterashima78 <sterashima78@gmail.com>
2020-03-02 12:16:10 +0000

    setting

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,3 +1,7 @@
 import CompositionApi from "@vue/composition-api";
 import Vue from "vue";
 Vue.use(CompositionApi);
+
+import * as VueWindow from "@hscmap/vue-window";
+
+Vue.use(VueWindow);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,28 @@
 <template>
   <div style="height: 100%">
-    <span
-      style="display: inline-block;padding: 5px;cursor: move"
-      v-for="component in components"
-      v-text="component"
-      :key="component"
-      draggable="true"
-      @dragstart="dragTag = component"
-    />
-    <div style="display:flex;width:100%;height:100%">
+    <v-app-bar color="deep-purple accent-4" dense dark>
+      <v-toolbar-title>Vue Webpage Builder</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="showTree = !showTree">
+        <v-icon>format_list_bulleted</v-icon>
+      </v-btn>
+      <v-btn icon @click="showWindow = !showWindow">
+        <v-icon>code</v-icon>
+      </v-btn>
+      <v-btn icon @click="showSetting = !showSetting">
+        <v-icon>settings</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <div style="display:flex;width:100%;height: calc(100% - 50px)">
       <VIframeSandbox
         :body="body"
-        style="width:80%;height:100%"
+        style="width:100%;height:100%"
         :script="inlineScript"
         :scriptsSrc="scriptsSrc"
         :styles="stylesStr"
         :cssLinks="cssLinks"
         @loaded="loaded"
       />
-      <v-treeview
-        :items="treeNode.children"
-        :hoverable="true"
-        style="width:20%;height:100%"
-      >
-        <template slot="label" slot-scope="{ item }">
-          <div
-            :id="item.id"
-            draggable="true"
-            style="cursor: move"
-            @mouseenter="cancelEvent"
-            @mouseover="mouseOver"
-            @mouseleave="mouseLeave"
-            @dragstart="dragStart"
-            @dragleave="dragLeave"
-            @dragenter="dragEnter"
-            @dragover="cancelEvent"
-            @dragend="dragEnd"
-            @drop="drop"
-          >
-            {{ item.name }}
-          </div>
-        </template>
-        <template #append="{ item }">
-          <v-icon @click="activeDialog(item.id)">tune</v-icon>
-          <v-icon @click="removeNodeById(item.id)">delete</v-icon>
-        </template>
-      </v-treeview>
     </div>
     <v-dialog v-model="dialog" persistent>
       <ElementEditor
@@ -55,6 +32,146 @@
         @close="dialog = false"
         @update="setEditTarget(editNode.id)"
       />
+    </v-dialog>
+    <hsc-window-style-black>
+      <hsc-window
+        v-show="showTree"
+        title="Componet Tree"
+        :resizable="true"
+        :isScrollable="true"
+        :minWidth="300"
+        :minHeight="300"
+        :maxWidth="500"
+        :maxHeight="600"
+      >
+        <v-treeview
+          :items="treeNode.children"
+          :hoverable="true"
+          style="width:100%;height:100%"
+        >
+          <template slot="label" slot-scope="{ item }">
+            <div
+              :id="item.id"
+              draggable="true"
+              style="cursor: move"
+              @mouseenter="cancelEvent"
+              @mouseover="mouseOver"
+              @mouseleave="mouseLeave"
+              @dragstart="dragStart"
+              @dragleave="dragLeave"
+              @dragenter="dragEnter"
+              @dragover="cancelEvent"
+              @dragend="dragEnd"
+              @drop="drop"
+            >
+              {{ item.name }}
+            </div>
+          </template>
+          <template #append="{ item }">
+            <v-icon @click="activeDialog(item.id)">tune</v-icon>
+            <v-icon @click="removeNodeById(item.id)">delete</v-icon>
+          </template>
+        </v-treeview>
+      </hsc-window>
+
+      <hsc-window
+        v-show="showWindow"
+        title="Components"
+        :resizable="true"
+        :isScrollable="true"
+        :minWidth="300"
+        :minHeight="300"
+        :maxWidth="500"
+        :maxHeight="600"
+      >
+        <span
+          style="display: inline-block;padding: 5px;cursor: move"
+          v-for="component in components"
+          v-text="component"
+          :key="component"
+          draggable="true"
+          @dragstart="dragTag = component"
+        />
+      </hsc-window>
+    </hsc-window-style-black>
+    <v-dialog
+      v-model="showSetting"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon dark @click="showSetting = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text @click="showSetting = false">Save</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list three-line subheader>
+          <v-subheader>User Controls</v-subheader>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Content filtering</v-list-item-title>
+              <v-list-item-subtitle
+                >Set the content filtering level to restrict apps that can be
+                downloaded</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Password</v-list-item-title>
+              <v-list-item-subtitle
+                >Require password for purchase or use password to restrict
+                purchase</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list three-line subheader>
+          <v-subheader>General</v-subheader>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Notifications</v-list-item-title>
+              <v-list-item-subtitle
+                >Notify me about updates to apps or games that I
+                downloaded</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Sound</v-list-item-title>
+              <v-list-item-subtitle
+                >Auto-update apps at any time. Data charges may
+                apply</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-checkbox></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Auto-add widgets</v-list-item-title>
+              <v-list-item-subtitle
+                >Automatically add home screen widgets</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </v-dialog>
   </div>
 </template>
@@ -151,7 +268,10 @@ export default defineComponent({
       dragEnd: dragEnd(dragNodeId),
       dragEnter: dragEnter(dropNodeId),
       drop: drop(dragTag, dragNodeId, dropNodeId, addNodeTo, moveNodeTo),
-      setEditTarget
+      setEditTarget,
+      showWindow: ref(true),
+      showTree: ref(true),
+      showSetting: ref(true)
     };
   }
 });
