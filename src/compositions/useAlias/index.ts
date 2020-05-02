1ba0853af6542b8d1ebb2a20022257f081541894
sterashima78 <sterashima78@gmail.com>
2020-05-02 12:40:22 +0000

    alias

---
+++ src/compositions/useAlias/index.ts
@@ -0,0 +1,22 @@
+import {
+  findByName,
+  create,
+  regist,
+  getAliasNames,
+  NodeAliasMap
+} from "@/domain/alias";
+import { Ref, ref, computed } from "@vue/composition-api";
+import { pipe } from "fp-ts/es6/pipeable";
+import { NodeTree } from "@/types";
+
+const alias: Ref<NodeAliasMap> = ref({});
+
+export const useAlias = () => {
+  return {
+    aliases: computed(() => pipe(alias.value, getAliasNames)),
+    regist: (name: string, node: NodeTree) =>
+      pipe(alias.value, regist(name, node)),
+    findByName: (name: string) => pipe(alias.value, findByName(name)),
+    create: (name: string) => pipe(alias.value, create(name))
+  };
+};
